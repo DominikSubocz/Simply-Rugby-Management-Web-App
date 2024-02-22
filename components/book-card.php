1339dbdad4096f6ed19d18586f4f8a67c866feaa
Dominik Subocz <dominiksubocz@gmail.com>
2024-02-22 12:11:34 +0000

    adding nice reponsivity and layouts

--- components/book-card.php
+++ components/book-card.php
@@ -1,29 +1,21 @@
 <a class="book-card" href="book.php?id=<?php echo $player_id; ?>">
   <div class="id-container card-container">
-      <h3><?php echo $player_id; ?></h3>
+      <p><?php echo $player_id; ?></p>
   </div>
 
   <div class="firstN-container card-container">
-      <h3><?php echo $firstName; ?></h3>
+      <p><?php echo $firstName; ?></p>
   </div>
   <div class="lastN-container card-container">
       <p><?php echo $lastName; ?></p>
   </div>
 
   <div class="sru-container card-container">
-      <p class="book-price">SRU:<?php echo $sruNumber; ?></p>
+      <p class="book-price"><?php echo $sruNumber; ?></p>
   </div>
   
   <div class="dob-container card-container">
       <p class="book-price"><?php echo $dob; ?></p>
-  </div>
-
-  <div class="contactNo-container card-container">
-      <p class="book-price"><?php echo $contactNumber; ?></p>
-  </div>
-
-  <div class="email-container card-container">
-      <p class="book-price"><?php echo $emailAddress; ?></p>
   </div>
 
   <div class="pfp-container card-container">
--- components/book-single.php
+++ components/book-single.php
@@ -1,121 +1,99 @@
 
 
-<div class="book-container">
-
-  <div class="pfp-container">
-    <img src="images/<?php echo $filename; ?>" alt="Cover of <?php echo $firstName; ?>" class="profile-img">
-    <div><button class="button profile-btn" onclick="displayTabs(0)">Personal Information</button></div>
-    <div><button class="button profile-btn" onclick="displayTabs(1)">Skills</button></div>
-  </div>
-
-
-
-
-    <div id="book-info" class="book-info tab">
-      <h2><?php echo $firstName, ' ', $lastName; ?></h2>
-      <h3><?php echo 'SRU:', $sruNumber; ?></h3>
-      <p class="book-price"><?php echo 'Contact Number: ', $contactNumber; ?></p>
-      <p class="book-price"><?php echo 'Mobile Number: ', $mobileNumber; ?></p>
-      <p class="book-price"><?php echo 'Email: ', $emailAddress; ?></p>
-      <p class="book-price"><?php echo 'Known Health Issues: ', $healthIssues; ?></p>
-      <p class="book-price"><?php echo 'Next of kin: ', $nextOfKin; ?></p>
-      <p class="book-price"><?php echo 'Mobile Number: ', $kinContactNumber; ?></p>
-      <h2> Address </h2>
-      <p class="book-price"><?php echo 'Address Line 1: ', $address1; ?></p>
-      <p class="book-price"><?php echo 'Address Line 2: ', $address2; ?></p>
-      <p class="book-price"><?php echo 'City: ', $city; ?></p>
-      <p class="book-price"><?php echo 'County: ', $county; ?></p>
-      <p class="book-price"><?php echo 'Postcode: ', $postcode; ?></p>
-    </div>
-    <div id="skills-info" class="idk-info tab">
-    <h2>Skills</h2>
-
-<div class="skills-card-container">
-
-  <div class="positions-card">
-      <h3>Positions</h3>
+<div class="player-info-card">
+  
+    <div class="pfp-container">
+      <img src="images/<?php echo $filename; ?>" alt="Cover of <?php echo $firstName; ?>" class="profile-img">
+      <div><button class="button profile-btn" onclick="displayTabs(0)">Personal Information</button></div>
+      <div><button class="button profile-btn" onclick="displayTabs(1)">Skills</button></div>
     </div>
     
-    <?php
-        // $skill = Book::getJuniorPositions($_GET["id"]);
-        // Components::juniorPositions($skill);
-    ?>
-
-</div>
-<br>
-<div class="skills-card passing-card">
   <div>
-    <br>
-    <h3>Passing</h3>
+    <div id="player-info" class="book-info tab">
+      <div>
+        <h2> Personal Details </h2>
+          <div class="basic-info-card profile-card">
+            <p><?php echo 'Name: ', $firstName, ' ', $lastName; ?></p>
+            <p><?php echo 'SRU: ', $sruNumber; ?></p>
+            <p ><?php echo 'Contact Number: ', $contactNumber; ?></p>
+            <p ><?php echo 'Mobile Number: ', $mobileNumber; ?></p>
+            <p ><?php echo 'Email: ', $emailAddress; ?></p>
+            <p ><?php echo 'Known Health Issues: ', $healthIssues; ?></p>
+          </div>
+
+          <div>
+            <h2>Emergency Contact</h2>
+            <div class="emergency-info-card profile-card">
+              <p ><?php echo 'Next of kin: ', $nextOfKin; ?></p>
+              <p ><?php echo 'Mobile Number: ', $kinContactNumber; ?></p>
+            </div>
+          </div>
+      </div>
+
+
+
+        <div>
+          <h2>Address Information</h2>
+          <div class="address-info-card profile-card">
+            <p ><?php echo 'Address Line 1: ', $address1; ?></p>
+            <p ><?php echo 'Address Line 2: ', $address2; ?></p>
+            <p ><?php echo 'City: ', $city; ?></p>
+            <p ><?php echo 'County: ', $county; ?></p>
+            <p ><?php echo 'Postcode: ', $postcode; ?></p>
+          </div>
+        </div>
+    </div>
   </div>
-
-    <?php
-        $skill = Book::getPlayerSkills($_GET["id"]);
-        Components::playerPassingSkill($skill);
-        ?>
-</div>
-
-<div class="skills-card tackling-card">
-  <div>
-  <br>
-    <h3>Tackling</h3>
-  </div>
-
-    <?php
-        $skill = Book::getPlayerSkills($_GET["id"]);
-        Components::playerTacklingSkill($skill);
-        ?>
-
-
-
-  </div>
-
-  <div class="skills-card tackling-card">
-  <div>
-  <br>
-    <h3>Kicking</h3>
-  </div>
-
-    <?php
-        $skill = Book::getPlayerSkills($_GET["id"]);
-        Components::playerKickingSkill($skill);
-        ?>
-
-
-
-</div>
-</div>
-
-      
-    <script>
-
-var currentTab = 0; // Current tab is set to be the first tab (0)
-
-x = document.getElementById("book-info");
-s = document.getElementById("skills-info");
-
-if (currentTab == 0){
-    x.style.display = "block";
-    s.style.display = "none";
-}
-
-function displayTabs(t){
-
-  if (t == 1){
-    s.style.display = "block";
-    x.style.display = "none";
-
-
-  }
-
-  else{
-    x.style.display = "block";
-    s.style.display = "none";
-  }
-
-}
-</script>
   
-
-
+  <div class="skills-info-card">  
+      <div id="skills-info" class="idk-info tab">
+    <h2> Skill Ratings </h2>
+  
+        <br>
+        <div class="skills-card skill-card-container passing-card">
+          <div>
+            <br>
+            <h3>Passing</h3>
+          </div>
+  
+            <?php
+                $skill = Book::getPlayerSkills($_GET["id"]);
+                Components::playerPassingSkill($skill);
+                ?>
+        </div>
+  
+        <div class="skills-card skill-card-container tackling-card">
+        <div>
+          <br>
+            <h3>Tackling</h3>
+          </div>
+  
+            <?php
+                $skill = Book::getPlayerSkills($_GET["id"]);
+                Components::playerTacklingSkill($skill);
+                ?>
+  
+  
+  
+          </div>
+  
+        <div class="skills-card skill-card-container kicking-card">
+            <div>
+            <br>
+              <h3>Kicking</h3>
+            </div>
+  
+            <?php
+              $skill = Book::getPlayerSkills($_GET["id"]);
+              Components::playerKickingSkill($skill);
+            ?>
+  
+  
+  
+        </div>
+      </div>
+  
+  
+  </div>
 </div>
+
